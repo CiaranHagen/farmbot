0577fbd5321788d3ba149e442bcae845f7557bf4
Ciaran Hagen <hagen.ciaran@gmail.com>
2018-11-18 20:41:15 +0100

    Changes to be committed: 	new file:   structure.py

---
+++ structure.py
@@ -0,0 +1,130 @@
+import time
+
+##CLASSES
+
+class PlantType():
+    def __init__(self, name, lightNeeded, waterNeeded, growthTimeS, growthTimeP, growthTimeF):
+        """
+        name : string
+        lightNeeded : int (lumen)
+        waterNeeded : int (ml/day)
+        growthTimeS : int (days)
+        growthTimeP : int (days)
+        growthTimeF : int (days)
+        """
+        self.name = name
+        self.lightNeeded = lightNeeded
+        self.waterNeeded = waterNeeded
+        self.growthTimeS = growthTimeS
+        self.growthTimeP = growthTimeP
+        self.growthTimeF = growthTimeF
+        
+class Plant():
+    growthStage = 0
+    daysInStage = 0
+    def __init__(self, kind, pot):
+        """
+        kind : PlantType
+        pot : Pot
+        """
+        self.kind = kind
+        self.pot = pot
+    
+class Pot():
+    plant = None 
+    full = False
+    def __init__(self, region):
+        """
+        region : Region
+        """
+        self.region = region
+    
+
+class region():
+    def __init__(self, gs, position):
+        """
+        gs : int
+        position : ((<x1>,<y1>),(<x2>,<y2>))
+        """
+        self.growthStage = gs
+        self.position = position
+ 
+##LIST AND VARIABLE INITIALIZATIONS
+PlantTypeList = [PlantType]     #plant type repository for accessing data for growth needs
+WaterList = {}                  #dict[time] = [Plant]  --> when to water which pot
+repotList = {}                  #dict[time] = [Plant]  --> when to repot a certain plant
+PlantList = [Plant]             #current plants
+
+##FUNCTIONS
+def currDate():
+    """
+    return current date as string in dd/mm/yyyy format
+    """
+    return str(time.localtime(time.time())[2]) + "/" + str(time.localtime(time.time())[1]) + "/" + str(time.localtime(time.time())[0])
+def currTime():
+    """
+    return current time as string in hh:mm format
+    """
+    return str(time.localtime(time.time())[3]) + ":" + str(time.localtime(time.time())[4])
+def UWaterList():
+    return
+def checkDead():
+    return
+def URepotList():
+    return
+def sendMail(kind):
+    me = "farmbot@neobuild.lu"
+    you = "ciaran.hagen.001@student.uni.lu"
+    if kind == 0:
+        textfile = "./plantsDonemsg.txt"
+        subject = "There are plants done."
+    elif kind == 1:
+        textfile = "./needPeatmsg.txt"
+        subject = "Some pots need new peat."
+    else:
+        textfile = "./errormsg.txt"
+        subject = "An error occurred."
+"""       
+    import smtplib
+
+    # Import the email modules we'll need
+    from email.mime.text import MIMEText
+
+    # Open a plain text file for reading.  For this example, assume that
+    # the text file contains only ASCII characters.
+    with open(textfile, 'rb') as fp:
+        # Create a text/plain message
+        msg = MIMEText(fp.read())
+
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = '%s' % subject
+    msg['From'] = me
+    msg['To'] = you
+
+    # Send the message via our own SMTP server, but don't include the
+    # envelope header.
+    s = smtplib.SMTP('localhost')
+    s.sendmail(me, [you], msg.as_string())
+    s.quit()
+"""  
+    
+sendMail(0)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
