8b96c921f239099770fd3af99562305bf25c1540
Ciaran Hagen <hagen.ciaran@gmail.com>
2018-11-26 21:38:58 +0100

    string instead of int as coords

--- FARMWARE.py
+++ FARMWARE.py
@@ -45,12 +45,12 @@ class MyFarmware():
             if self.input_debug >= 1: log(('Move absolute: ' + str(x) + ", " + str(y) + ", " + str(z) + ", " + str(spd)) , message_type='debug', title=str(self.farmwarename) + ' : move_absolute_point')
             if self.input_debug < 2: 
                 self.check_celerypy(move_absolute(
-                    location=[x,y,z],
+                    location=[str(x),str(y),str(z)],
                     offset=[0, 0, 0],
                     speed=spd))
     
     
     def run(self):
         log("test")
-        self.move_absolute(0.0,0.0,100.0,800)
+        self.move_absolute(0,0,100,800)
         
