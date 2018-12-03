8defbb96084fce18d0d77ceceb03acab131abefc
Ciaran Hagen <hagen.ciaran@gmail.com>
2018-12-03 11:44:24 +0100

    halp

--- launch.py
+++ launch.py
@@ -3,10 +3,10 @@ from FARMWARE import MyFarmware
 from farmware_tools import log
 
 FARMWARE_NAME = "jhempbot"
-
+"""
 def get_env(key, type_=str):
     return type_(os.environ["{}_{}".format(FARMWARE_NAME, key)])
-
+"""
 def main():
     log("jhempbot --> hello")
     #farmware = MyFarmware(FARMWARE_NAME)
