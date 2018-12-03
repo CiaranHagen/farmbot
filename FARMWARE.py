4119d619d7bb74d8f50b9efec7f80fe9cbf31ae7
Ciaran Hagen <hagen.ciaran@gmail.com>
2018-12-03 11:28:11 +0100

    debugging

--- FARMWARE.py
+++ FARMWARE.py
@@ -7,11 +7,11 @@ class MyFarmware():
     def __init__(self,farmwarename):
         self.farmwarename = farmwarename
 
-    def move_absolute_point(self,point, spd):
-            log("goint to " + point, message_type='debug', title=self.farmwarename)
+    def move(self,point, spd):
+        log("going to " + point, message_type='debug')
     
     
     def run(self):
-        log("test", message_type='debug', title=self.farmwarename)
-        self.move_absolute([-100, -100, 50], 1)
+        log("test", message_type='debug')
+        self.move([-100, -100, 50], 1)
         
--- launch.py
+++ launch.py
@@ -7,7 +7,7 @@ import sys
 FARMWARE_NAME = "jhempbot"
 
 def get_env(key, type_=str):
-    return type_(os.environ["{}_{}".format(farmware_name, key)])
+    return type_(os.environ["{}_{}".format(FARMWARE_NAME, key)])
 
 def main():
     log("jhempbot --> hello")
@@ -16,36 +16,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-"""
-
-if __name__ == "__main__":
-
-    FARMWARE_NAME = "jhempbot"
-    #FARMWARE_NAME = ((__file__.split(os.sep))[len(__file__.split(os.sep))-3]).replace('-master','')
-
-    log('Starting farmware...', message_type='info', title=FARMWARE_NAME)
-    
-    try:
-        reload(sys)
-        sys.setdefaultencoding('utf8') #force utf8 for celerypy return code
-        log('Setting encoding.', message_type='info', title=FARMWARE_NAME)
-    except:
-        pass
-
-    try:
-        farmware = MyFarmware(FARMWARE_NAME)
-        #log('initializing farmware', message_type='info', title=FARMWARE_NAME)
-    except Exception as e:
-        log(e ,message_type='error', title=FARMWARE_NAME + " : init" )
-        raise Exception(e) #Envoyer un message d'erreur -> arrète le programme
-    else:
-        try:
-            #log('farmware runs after this message.', message_type='info', title=FARMWARE_NAME)
-            farmware.run()
-        except Exception as e:
-            log(e ,message_type='error', title=FARMWARE_NAME + " : run" )
-            raise Exception(e)
-
-
-    log('Ending farmware...', message_type='info', title=FARMWARE_NAME)
-"""
