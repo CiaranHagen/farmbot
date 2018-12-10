d19baf215d97ea515ff974761e177f4d4b99f234
Ciaran Hagen <hagen.ciaran@gmail.com>
2018-12-10 11:48:06 +0100

    made the execution into a variable

--- FARMWARE.py
+++ FARMWARE.py
@@ -93,8 +93,6 @@ class MyFarmware():
         s.add(self.move(150, 150, -50, 50))
         x = send_celery_script(cp.create_node(kind='execute', args=s.sequence))
         
-        sleep(20)
-        
         a = Sequence("2", "green")
         a.add(self.move(100, 100, -100, 50))
         send_celery_script(cp.create_node(kind='execute', args=a.sequence))
