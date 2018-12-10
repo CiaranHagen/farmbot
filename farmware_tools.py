edebad69a11301e97cfe38a941a1d33b8da55a4e
Ciaran Hagen <hagen.ciaran@gmail.com>
2018-12-10 10:29:11 +0100

    AAAAAAAAAAAAAAAAAAAAAAHHHHHHHHH

--- farmware_tools.py
+++ farmware_tools.py
@@ -12,29 +12,26 @@ from functools import wraps
 debug = False
 
 def send_celery_script(command):
-    @wraps(command)
-    def wrapper(command):
-        'Send a celery script command.'
-        #start debug
-        if debug == True:
-            print(command)
-            if command["kind"] == "wait":
-                sleep(command["args"]["milliseconds"]//1000)
-            return
-        #end debug
-        try:
-            url = os.environ['FARMWARE_URL']
-            token = os.environ['FARMWARE_TOKEN']
-        except KeyError:
-            print(command)
-        else:
-            ret = requests.post(
-                url + 'api/v1/celery_script',
-                headers={'Authorization': 'Bearer ' + token,
-                         'content-type': 'application/json'},
-                data=json.dumps(command))
-            return ret
-    return wrapper(command)
+    'Send a celery script command.'
+    #start debug
+    if debug == True:
+        print(command)
+        if command["kind"] == "wait":
+            sleep(command["args"]["milliseconds"]//1000)
+        return
+    #end debug
+    try:
+        url = os.environ['FARMWARE_URL']
+        token = os.environ['FARMWARE_TOKEN']
+    except KeyError:
+        print(command)
+    else:
+        ret = requests.post(
+            url + 'api/v1/celery_script',
+            headers={'Authorization': 'Bearer ' + token,
+                     'content-type': 'application/json'},
+            data=json.dumps(command))
+        return ret
 
 def log(message, message_type='info'):
     'Send a send_message command to post a log to the Web App.'
