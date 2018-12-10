9e91368ff763effa5683e860ca570e4205b1e4d7
Ciaran Hagen <hagen.ciaran@gmail.com>
2018-12-10 09:12:34 +0100

    changed the send_celery_script command

--- farmware_tools.py
+++ farmware_tools.py
@@ -11,7 +11,7 @@ from time import sleep
 
 debug = False
 
-"""
+
 def send_celery_script(command):
     'Send a celery script command.'
     #start debug
@@ -35,22 +35,7 @@ def send_celery_script(command):
                        'content-type': "application/json"},
             data=json.dumps(command))
     return True
-"""
 
-def send_celery_script(ret):
-        try:
-            status_code = ret.status_code
-        except:
-            status_code = -1
-        try:
-            text = ret.text[:100]
-        except:
-            text = ret
-        if status_code == -1 or status_code == 200:
-            if self.input_debug >= 1: log("{} -> {}".format(status_code,text), message_type='debug', title="jhempbot")
-        else:
-            log("{} -> {}".format(status_code,text), message_type='error', title="jhempbot")
-            raise
 
 def log(message, message_type='info'):
     'Send a send_message command to post a log to the Web App.'
